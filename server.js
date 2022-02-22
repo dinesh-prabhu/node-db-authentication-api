9f36c8d9978dd39f7ab30a18915b5037d6641b9a
dinesh-prabhu <dineshprabhu.s@outlook.com>
2022-02-23 00:51:02 +0530

    Initiated db connection on app startup

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
+import mongoose from "mongoose";
 import { createRequire } from "module";
+import authRouter from "./routes/auth.js";
 
 const require = createRequire(import.meta.url);
 const app = express();
@@ -7,11 +9,13 @@ const app = express();
 const config = require("./config.json");
 const PORT = config.development.port || 3000;
 
-app.get("/", (req, res) => {
-    res.send({
-        message: "Hello World"
-    });
-});
+app.use("/", authRouter);
+
+const connect = async () => {
+    await mongoose.connect(config.development.db_url);
+}
+
+connect().catch(err => console.log(err));
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
