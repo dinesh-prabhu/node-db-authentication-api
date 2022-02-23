68d71d68f54f0e4316177a73a3072d4d80a9117b
dinesh-prabhu <dineshprabhu.s@outlook.com>
2022-02-24 01:42:04 +0530

    Added password field and toJSON method to the schema.

--- models/User.js
+++ models/User.js
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     age: Number,
-    email:{ type: String, required: true, unique: true }
+    email: { type: String, required: true, unique: true },
+    password: {type: String}
 });
 
+userSchema.set("toJSON", {
+    versionKey: false,
+    transform: function(_doc, ret) {
+        return {
+            firstName: ret.firstName,
+            email: ret.email
+        }
+    }
+})
 const User = mongoose.model("User", userSchema);
 export default User;
