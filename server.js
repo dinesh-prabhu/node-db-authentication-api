b1ba86deed38d69a548a30efe53e80ee0207719a
dinesh-prabhu <dineshprabhu.s@outlook.com>
2022-02-24 01:40:34 +0530

    Removed createRequire usage.

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
-import { createRequire } from "module";
+import config from "./config.js";
 import authRouter from "./routes/auth.js";
 
-const require = createRequire(import.meta.url);
 const app = express();
-
-const config = require("./config.json");
-const PORT = config.development.port || 3000;
+const PORT = config.port || 3000;
 
 app.use("/", authRouter);
 
 const connect = async () => {
-    await mongoose.connect(config.development.db_url);
+    await mongoose.connect(config.db_url);
 }
-
 connect().catch(err => console.log(err));
 
 app.listen(PORT, () => {
