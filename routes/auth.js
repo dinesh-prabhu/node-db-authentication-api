ca6323a0d67c5b5156c6d4848b6d3a194d2ee308
dinesh-prabhu <dineshprabhu.s@outlook.com>
2022-02-24 01:42:58 +0530

    Added login and used hash to verify credential.

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,55 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import config from "../config.js";
 import User from "../models/User.js";
 
 const router = express.Router();
 
-const checkAndRegisterUser = async (data) => {
-    let user = await User.exists({email: data.email});
+const checkAndRegisterUser = async ({ firstName, lastName, email, age, password }) => {
+    let user = await User.exists({ email });
+    let res = {};
     if(user && "_id" in user) {
-        return {
-            status: "error",
-            message: "User already exists!"
-        }
+        res.status = "error";
+        res.message = "User already exists.";
+    } else {
+        let hash = await bcrypt.hash(password, 10);
+        user = new User({ firstName, lastName, email, age, password: hash });
+        await user.save();
+        res.status = "success";
     }
-    user = new User({
-        name: data.name,
-        email: data.email,
-        age: data.age
-    });
-    await user.save();
-    return user;
+    return res;
 }
 
-router.use("/register", express.json());
+const login = async ({ email, password }) => {
+    let res = {};
+    try {
+        let user = await User.findOne({ email });
+        console.log(user);
+        if(!user) {
+            res.status = "error";
+            res.message = "User not found.";
+        } else if(await bcrypt.compare(password, user.password)) {
+            res.status = "success";
+            res["access-token"] = jwt.sign(user.toJSON(), config.JWT_KEY, { expiresIn: "1h" });
+        } else {
+            res.status = "error";
+            res.message = "Invalid password.";
+        }
+    } catch(err) {
+        res.status = "error";
+        res.message = err.message;
+    }
+    return res;
+}
+
+router.use(express.json());
+
 router.post("/register", async (req, res) => {
-    let resJSON = await checkAndRegisterUser(req.body);
-    res.send(resJSON);
+    res.send(await checkAndRegisterUser(req.body));
+});
+router.post("/login", async (req, res) => {
+    res.send(await login(req.body));
 });
 
 export default router;
