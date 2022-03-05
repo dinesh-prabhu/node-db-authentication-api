76bfa8de4c3b845b0ef5ebd778c786de4a433419
dinesh-prabhu <dineshprabhu.s@outlook.com>
2022-03-05 12:23:30 +0530

    Abstracted auth and validate functionality.

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,24 @@
 import express from "express";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import config from "../config.js";
 import User from "../models/User.js";
+import authHandler from "../lib/AuthHandler.js";
+import PasswordAuth from "../lib/PasswordAuth.js";
 
 const router = express.Router();
+authHandler.useAuth(new PasswordAuth({ userNameField: "email"}, async (email, password, callback) => {
+    try {
+        let user = await User.findOne({ email });
+        if(!user) {
+            throw new Error("User not found.");
+        } else if(await bcrypt.compare(password, user.password)) {
+            callback(null, user.toJSON())
+        } else {
+            throw new Error("Invalid password.");
+        }
+    } catch(err) {
+        callback(err, null);
+    }
+}));
 
 const checkAndRegisterUser = async ({ firstName, lastName, email, age, password }) => {
     let user = await User.exists({ email });
@@ -21,35 +35,18 @@ const checkAndRegisterUser = async ({ firstName, lastName, email, age, password 
     return res;
 }
 
-const login = async ({ email, password }) => {
-    let res = {};
-    try {
-        let user = await User.findOne({ email });
-        console.log(user);
-        if(!user) {
-            res.status = "error";
-            res.message = "User not found.";
-        } else if(await bcrypt.compare(password, user.password)) {
-            res.status = "success";
-            res["access-token"] = jwt.sign(user.toJSON(), config.JWT_KEY, { expiresIn: "1h" });
-        } else {
-            res.status = "error";
-            res.message = "Invalid password.";
-        }
-    } catch(err) {
-        res.status = "error";
-        res.message = err.message;
-    }
-    return res;
-}
-
 router.use(express.json());
+router.use(express.static('public'));
+router.use(authHandler.verifyToken({
+    nonSecurePaths: ["/login", "/authenticate", "/register"]
+}));
 
+router.get("/login", (req, res) => {
+    res.redirect("/index.html");
+});
 router.post("/register", async (req, res) => {
     res.send(await checkAndRegisterUser(req.body));
 });
-router.post("/login", async (req, res) => {
-    res.send(await login(req.body));
-});
+router.post("/authenticate", authHandler.authenticate());
 
 export default router;
